9ca39f305f2884f007e961d01b6efa2c206a1092
Alexander Wasserman <61466759+xanderwasserman@users.noreply.github.com>
2025-03-18 08:59:27 +0100

    Create header file for read_p1 module

---
+++ read_p1.h
@@ -0,0 +1,8 @@
+#pragma once
+
+unsigned int crc16(unsigned int crc, unsigned char *buf, int len);
+bool isNumber(char *res, int len);
+int findCharInArrayRev(char array[], char c, int len);
+long getValue(char *buffer, int maxlen, char startchar, char endchar);
+bool decodeTelegram(int len);
+bool readP1Serial();
